5b3359b66a3f3b720e98dca67d7011e2ce12c50a
Inny So <inny.so@digital.cabinet-office.gov.uk>
2015-06-24 17:13:22 +0100

    Add flip that table command

--- cli.js
+++ cli.js
@@ -5,12 +5,15 @@ var program = require('commander');
 
 program
 .option('-a, --all', 'List all cool faces')
+.option('-F, --flip-that-table','FLIP THAT TABLE')
 .parse(process.argv);
 
 if (program.all){
   coolFace.faces.forEach(function (face) {
     process.stdout.write(face + '\n');
   });
+}else if (program.flipThatTable){
+  process.stdout.write("flip that table\n");
 }else{
   process.stdout.write(coolFace() + '\n');
 }
